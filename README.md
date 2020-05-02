c27eb9f84312bda801ad4ad1ec67bd5f46ec48eb
Ashutosh Pandey <43195949+ashutoshpandey1490@users.noreply.github.com>
2020-05-02 17:37:26 +0100

    Initial project setup.
    Initial project setup. In memory DB added with data.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # graphql-spring-boot-dataloader-example
 Sample application for GraphQL, Spring boot and DataLoader.
+
+This application is aimed to provide integration for GraphQL, Spring boot and Dataloader.
---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO PERSONS (first_name, last_name, email, phone_no) VALUES
+  ('Ashutosh', 'Pandey', 'abc@gmail.com', '1234'),
+  ('abc', 'xyz', 'xyz@email.com', '2345'),
+  ('Caption', 'America', 'cap@marvel.com', '4567'),
+  ('Ashutosh', 'Pandey', 'abc@gmail.com', '1234'),
+  ('abc', 'xyz', 'xyz@email.com', '2345'),
+  ('Caption', 'America', 'cap@marvel.com', '4567'),
+  ('Ashutosh', 'Pandey', 'abc@gmail.com', '1234'),
+  ('abc', 'xyz', 'xyz@email.com', '2345'),
+  ('Caption', 'America', 'cap@marvel.com', '4567'),
+  ('Ashutosh', 'Pandey', 'abc@gmail.com', '1234'),
+  ('abc', 'xyz', 'xyz@email.com', '2345'),
+  ('Caption', 'America', 'cap@marvel.com', '4567');
